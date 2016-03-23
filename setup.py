2ada49dee781b8dd5eb55905974b50fedee2bdfd
mjaworski <mjaworski@opera.com>
2016-03-23 12:18:31 +0100

    tests: extend test matrix in tox and CI and add missing trove classifiers

--- setup.py
+++ setup.py
@@ -82,7 +82,12 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',  # noqa
     ],
 
