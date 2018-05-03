5aa661976c35e2eee2e8fdd1d1cd582cd5fdf370
Michał Jaworski <mjaworski@vewd.com>
2018-05-03 14:26:40 +0200

    data dir for consul

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,13 +9,14 @@ CONSUL_BIN = os.environ.get(
     "CONSUL_BIN",
     os.path.join(os.path.dirname(os.path.dirname(__file__)), 'consul')
 )
+CONSUL_DATA_DIR = os.environ.get('CONSUL_DATA_DIR', '/tmp/consul')
 
 
 @pytest.fixture(scope="session")
 def consul_instance():
 
-    command = '{bin} agent -server -data-dir /tmp/consul -bootstrap-expect=1' \
-              ''.format(bin=CONSUL_BIN)
+    command = '{bin} agent -server -data-dir {datadir} -bootstrap-expect=1' \
+              ''.format(bin=CONSUL_BIN, datadir=CONSUL_DATA_DIR)
 
     command = command.format(bin=bin).strip()
     command = shlex.split(command)
